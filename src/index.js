b1ff38d5e83016b93649b78895092937513798de
Tiago Rangel <tr@tiagorangel.com>
2024-12-26 17:29:03 +0000

    set version to v0.1.0

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Worker } from "worker_threads";
 import fs from "fs/promises";
 import os from "os";
 
-const __VERSION = "v0.0.6";
+const __VERSION = "v0.1.0";
 const __REPO = "https://github.com/tiagorangel1/bunbuster";
 
 if (!process.versions.bun) {
